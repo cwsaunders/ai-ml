66e4e7feff86f3e7fe460dfb6cdeec2d71c471e4
noname45688@gmail.com <csaunders5@liberty.edu>
2020-07-27 03:57:56 -0400

    notes

--- educational/tensorflow/driver.py
+++ educational/tensorflow/driver.py
@@ -11,3 +11,10 @@ string = tf.Variable("this is a string", tf.string)
 number = tf.Variable(333,tf.int16)
 float = tf.Variable(333.33,tf.float64)
 '''
+
+# Tensor ranks
+'''
+Rank 0: 1 object. (i.g number = tf.Variable(333,tf.int16))
+Rank 1: 1 list/array (i.g array = tf.Variable([333,356,222],tf.int16))
+Rank 2: List/array within list/array (i.g array = tf.Variable([[333,356,222],[333,356,222]],tf.int16))
+'''
