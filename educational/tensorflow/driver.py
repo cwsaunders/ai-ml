9c754534b4301c962c8d5d6fa2f2eb5d6a5255fb
noname45688@gmail.com <csaunders5@liberty.edu>
2020-07-27 04:24:13 -0400

    notes

--- educational/tensorflow/driver.py
+++ educational/tensorflow/driver.py
@@ -31,14 +31,27 @@ Describes number of elements within array/list of tensor. Also describes number 
 i.g:
 array = tf.Variable([[333,356,222],[333,356,222][22,56,12]],tf.int16) == [3,3]
 
-first 3: number of lists
+first 3: number of interior lists
 second 3: number of elements within the lists
 '''
 # Reshaping Tensors
 '''
-tensor1 = tf.ones([1,2,3])
-tensor2 = tf.reshape(tensor1,[2,3,1])
-tensor3 = tf.reshape(tensor2,[3,-1])
+tensor1 = tf.ones([1,2,3]) <-- 'ones' creates a tensor full of ones in the described shape. in this case 1,2,3
+i.g [[[1,1,1],[1,1,1]]]
+One interior list containing our two lists of elements. Those lists contain three elements each. This explains 1,2,3
+tensor2 = tf.reshape(tensor1,[2,3,1]) <-- reshapes data, this produces:
+[
+    [1]
+    [1]
+    [1]]
+[
+    [1]
+    [1]
+    [1]
+]
+i.g 2 lists containing 3 interior lists containing 1 element each
+tensor3 = tf.reshape(tensor2,[3,-1]) <-- reshapes data, this produces:
+
 
 print(tensor1)
 print(tensor2)
