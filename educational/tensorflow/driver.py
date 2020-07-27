76093c538ae692167b7b4d3cc8e19387caeff81c
noname45688@gmail.com <csaunders5@liberty.edu>
2020-07-27 04:44:30 -0400

    notes and code in use

--- educational/tensorflow/driver.py
+++ educational/tensorflow/driver.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 '''
 https://www.tensorflow.org/
 https://www.youtube.com/watch?v=tPYj3fFJGjk
+https://colab.research.google.com/drive/1F_EWVKa8rbMXi3_fG0w7AtcscFq7Hi7B#forceEdit=true&sandboxMode=true&scrollTo=duDj86TfWFof
 '''
 
 # Version
