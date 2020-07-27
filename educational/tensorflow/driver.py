00871ab9ee712fde853a80d9e5f15b76c8cae5ad
noname45688@gmail.com <csaunders5@liberty.edu>
2020-07-27 03:53:59 -0400

    notes

--- educational/tensorflow/driver.py
+++ educational/tensorflow/driver.py
@@ -1,3 +1,13 @@
 import tensorflow as tf
 
+# Version
+'''
 print(tf.version)
+'''
+
+# Creating Tensors
+'''
+string = tf.Variable("this is a string", tf.string)
+number = tf.Variable(333,tf.int16)
+float = tf.Variable(333.33,tf.float64)
+'''
