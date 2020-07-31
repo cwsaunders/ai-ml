9df55105442b73f1245c88626a29595ac272c088
noname45688@gmail.com <csaunders5@liberty.edu>
2020-07-31 13:56:41 -0400

    data points/frames

--- educational/tensorflow/classification.py
+++ educational/tensorflow/classification.py
@@ -1,3 +1,22 @@
 import tensorflow as tf
 import pandas as pd
 
+# Classification Tensorflow model to guess flower species
+
+# Data points
+CSV_COLUMN_NAMES = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'Species']
+# Species
+SPECIES = ['Setosa', 'Versicolor', 'Virginica']
+
+# Reading in data using Keras -- Then transferring data into Pandas dataframe
+train_path = tf.keras.utils.get_file(
+    "iris_training.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv")
+test_path = tf.keras.utils.get_file(
+    "iris_test.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv")
+
+# Training / testing
+train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
+test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
+
+
+
