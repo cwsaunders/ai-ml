34e3f357e207fc8e079423d45a0504b29f035111
noname45688@gmail.com <csaunders5@liberty.edu>
2020-07-31 13:05:12 -0400

    create model and add training

--- educational/tensorflow/driver.py
+++ educational/tensorflow/driver.py
@@ -183,5 +183,16 @@ def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32)
     return ds  # return a batch of the dataset
   return input_function  # return a function object for use
 
+# Create objects of make_input_fn for training
 train_input_fn = make_input_fn(dftrain, y_train)  # here we will call the input_function that was returned to us to get a dataset object we can feed to the model
 eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+# Creating the model
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+
+# Training the model
+linear_est.train(train_input_fn)  # train
+result = linear_est.evaluate(eval_input_fn)  # get model metrics/stats by testing on tetsing data
+
+clear_output()  # clears consoke output
+print(result['accuracy'])  # the result variable is simply a dict of stats about our model
