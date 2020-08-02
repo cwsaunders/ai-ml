951e099101501170a756009c25fff62fe3869b0c
noname45688@gmail.com <csaunders5@liberty.edu>
2020-08-02 12:25:07 -0400

    Training model and outputting accuracy

--- educational/tensorflow/classification.py
+++ educational/tensorflow/classification.py
@@ -58,3 +58,33 @@ classifier = tf.estimator.DNNClassifier(
 classifier.train(
     input_fn=lambda: input_fn(train, train_y, training=True),
     steps=5000)
+
+eval_result = classifier.evaluate(input_fn=lambda: input_fn(test, test_y, training=False))
+print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+
+# Predictions
+'''
+def input_fn(features, batch_size=256):
+    # Convert the inputs to a Dataset without labels.
+    return tf.data.Dataset.from_tensor_slices(dict(features)).batch(batch_size)
+
+features = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth']
+predict = {}
+
+print("Please type numeric values as prompted.")
+for feature in features:
+  valid = True
+  while valid: 
+    val = input(feature + ": ")
+    if not val.isdigit(): valid = False
+
+  predict[feature] = [float(val)]
+
+predictions = classifier.predict(input_fn=lambda: input_fn(predict))
+for pred_dict in predictions:
+    class_id = pred_dict['class_ids'][0]
+    probability = pred_dict['probabilities'][class_id]
+
+    print('Prediction is "{}" ({:.1f}%)'.format(
+        SPECIES[class_id], 100 * probability))
+'''
