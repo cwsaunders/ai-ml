89d0d4f52e53dfa54e233c12ece96ef997534477
noname45688@gmail.com <csaunders5@liberty.edu>
2020-08-03 10:19:47 -0400

    hidden markov model notes

--- educational/tensorflow/hidden_markov.py
+++ educational/tensorflow/hidden_markov.py
@@ -2,5 +2,27 @@ import tensorflow as tf
 
 # Hidden Markov Models
 '''
+Hidden Markov Models
+"The Hidden Markov Model is a finite set of states, each of which is associated with a (generally multidimensional)
+probability distribution []. Transitions among the states are governed by a set of probabilities called transition probabilities."
+(http://jedlik.phy.bme.hu/~gerjanos/HMM/node4.html)
+A hidden markov model works with probabilities to predict future events or states.
+In this section we will learn how to create a hidden markov model that can predict the weather.
+https://www.tensorflow.org/probability/api_docs/python/tfp/distributions/HiddenMarkovModel
+'''
+
+# States
+'''
+Warm vs cold, high vs low, etc.
+'''
+
+# Observation
+'''
+Each state has a particular outcome or observation associated with it based on a probability distribution. i.g on a hot day Tim has
+a 60% chance of being happy and a 40% chance of being sad.
+'''
+
+# Transition
+'''
 
 '''
