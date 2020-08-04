e072bb7473398f392e2da4ac8aab23742bb7c7a6
noname45688@gmail.com <csaunders5@liberty.edu>
2020-08-04 16:01:02 -0400

    notes

--- educational/resources/hands_on_ml_notes.py
+++ educational/resources/hands_on_ml_notes.py
@@ -43,4 +43,33 @@ their ranking. A decrease in performance may also simply be due to bad data. Act
 3. reverting to a prior state
 '''
 
-# 
+# Example code to create GDP to happiness model
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sklearn.linear_model
+
+# Load the data
+oecd_bli = pd.read_csv("oecd_bli_2015.csv", thousands=',')
+gdp_per_capita = pd.read_csv("gdp_per_capita.csv",thousands=',',delimiter='\t',
+encoding='latin1', na_values="n/a")
+
+# Prepare data
+country_stats = prepare_country_stats(oecd_bli, gdp_per_capita)
+X = np.c_[country_stats["GDP per capita"]]
+y = np.c_[country_stats["Life satisfaction"]]
+
+# Visualize data
+country_stats.plot(kind='scatter', x="GDP per capita", y='Life satisfaction')
+plt.show()
+
+# Select linear model
+model = sklean.linear_model.LinearRegression()
+
+# Train the model
+model.fit(X,y)
+
+# Make a prediction for Cyprus
+X_new = [[22587]] # Cyprus GDP / capita
+print(model.predict(X_new)) # Output 
