f5d064a8895f2822144c7f3d86186b6c4bd0f71e
noname45688@gmail.com <csaunders5@liberty.edu>
2020-08-04 11:59:22 -0400

    notes

--- educational/resources/hands_on_ml_notes.py
+++ educational/resources/hands_on_ml_notes.py
@@ -1,2 +1,21 @@
 # Hands-On Machine Learning with Scikit-Learn, Keras, and TensorFlow Notes
 
+# Other resources
+'''
+Other resources to look into:
+1. Geoffrey Hinton’s course on neural networks and Deep Learning
+2. Data Science from Scratch, Joel Grus
+3. kaggle.com (ML competition website)
+'''
+
+# Conventions used within the book
+'''
+The following typographical conventions are used in this book:
+1. Italic Indicates new terms, URLs, email addresses, filenames, and file extensions.
+
+2. Constant width Used for program listings, as well as within paragraphs to refer to program 
+elements such as variable or function names, databases, data types, environment variables, statements and keywords.
+
+3. Constant width bold Shows commands or other text that should be typed literally by the user.
+4. Constant width italic Shows text that should be replaced with user-supplied values or by values determined by context.
+'''
