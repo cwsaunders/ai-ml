8a5c3194978449c1261a01ca8810732f19aeafdf
noname45688@gmail.com <csaunders5@liberty.edu>
2020-08-05 11:45:51 -0400

    notes mit

--- educational/resources/MIT_deep_learning.py
+++ educational/resources/MIT_deep_learning.py
@@ -5,3 +5,28 @@
 How does it work?
 Take your inputs, apply dot product with weights, add a bias, and apply a non-linearity
 '''
+
+# Working with weights
+'''
+Explanation around 33:00 
+
+Essentially weights are randomized and slowly creep in the right direction until finding the optimal weight that
+creates optimal output
+
+
+Code:
+
+import tensorflow as tf
+
+weights = tf.Variable([tf.random.normal()])
+
+while True:
+    with tf.GradientTape() as g:
+        loss = compute_loss(weights)
+        gradient = g.gradient(loss,weights)
+    
+    weights = weights - lr * gradient
+
+'''
+
+# 
