1d91377fb1ee0417b18c15bdafcbda2f149c5754
noname45688@gmail.com <csaunders5@liberty.edu>
2020-08-05 10:47:28 -0400

    notes mit

--- educational/resources/MIT_deep_learning.py
+++ educational/resources/MIT_deep_learning.py
@@ -1,2 +1,7 @@
 # MIT Introduction to Deep Learning Course Notes
 
+# Perceptron / Neuron
+'''
+How does it work?
+Take your inputs, apply dot product with weights, add a bias, and apply a non-linearity
+'''
