5badb6e5f71b93b3881002d55e2f950b3072fd01
noname45688@gmail.com <csaunders5@liberty.edu>
2020-08-05 11:56:31 -0400

    notes mit

--- educational/resources/MIT_deep_learning.py
+++ educational/resources/MIT_deep_learning.py
@@ -29,4 +29,24 @@ while True:
 
 '''
 
-# 
+# Setting the learning rate
+'''
+Too big: constantly overshooting
+Too small: being stuck in current position
+
+How to set a learning rate that is 'just right'?
+'Adaptive learning rate':
+Non-fixed learning rate. Made smaller or larger based on:
+1. how large gradient is
+2. how fast learning is happening
+3. size of particular weights
+4. etc.
+
+Implementation:
+SGD: tf.keras.optimizers.SGD -- (NON-ADAPTIVE)
+Adam: tf.keras.optimizers.Adam -- (ADAPTIVE)
+Adadelta: tf.keras.optimizers.Adadelta -- (ADAPTIVE)
+Adagrad: tf.keras.optimizers.Adagrad -- (ADAPTIVE)
+RMSProp: tf.keras.optimizers.RMSProp -- (ADAPTIVE)
+
+'''
