789ec371ef67469bec595f32f96b578ab74ac6cd
noname45688@gmail.com <csaunders5@liberty.edu>
2020-08-05 15:41:05 -0400

    notes

---
+++ educational/resources-and-notes/deeplearning_ai_notes.py
@@ -0,0 +1,25 @@
+# Deeplearning.ai Coursera Notes
+
+# Note resource
+'''
+https://www.slideshare.net/TessFerrandez/notes-from-coursera-deep-learning-courses-by-andrew-ng?trk=v-feed&lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_recent_activity_details_shares%3Ba1Vq7AYlTOmwFz%2FVvzR2wA%3D%3D&licu=urn%3Ali%3Acontrol%3Ad_flagship3_profile_view_base_recent_activity_details_shares-object
+'''
+
+# Logistic Regression
+'''
+An algorithm for binary classification (1 represents that an item is x and 0 represents that it is not x)
+C1W2L01 explains binary classification in detail
+Used when Y = 0 or Y = 1 (e.g binary classification problems)
+
+
+'''
+
+# Notation for course
+'''
+Video C1W2L01 explains. Also available online.
+w = n dimensional vector (weights)
+b = real number (regression coefficients)
+
+'''
+
+# 
