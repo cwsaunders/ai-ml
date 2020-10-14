a361a5c643fb67e541a90941163c6d775d63dc1c
noname45688@gmail.com <csaunders5@liberty.edu>
2020-10-14 14:45:57 -0400

    hands on ml

---
+++ educational/labs/hands_on_ml_exercises/chp5_exercises.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+X = 2*np.random.rand(100,1) # generate random data
+y = 4 + 3 * X + np.random.rand(100,1) # generate random data
+
+X_b = np.c_[np.ones((100,1)), X] # add xTHETA = 1 to each instance
+theta_best = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y) # calculate theta hat using normal equation
