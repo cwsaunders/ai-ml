1579a3191a81336636090903b1a55da9bba7423e
noname45688@gmail.com <csaunders5@liberty.edu>
2020-10-22 23:00:48 -0400

    hands on

--- educational/labs/hands_on_ml_exercises/chp5_exercises.py
+++ educational/labs/hands_on_ml_exercises/chp5_exercises.py
@@ -55,3 +55,24 @@ for iteration in range(n_iterations):
     theta = theta - eta * gradients
 
 print("theta:\n", theta)
+
+# Stochastic gradient descent (e.g randomized gradient descent)
+n_epochs = 50
+t0,t1 = 5,50
+
+def learning_schedule(t):
+    return t0/(t+t1)
+
+theta = np.random.randn(2,1)
+
+for epoch in range(n_epochs):
+    for i in range(m):
+        random_index = np.random.randint(m)
+        xi = X_b[random_index:random_index+1]
+        yi = y[random_index:random_index+1]
+        gradients = 2*xi.T.dot(xi.dot(theta) - yi)
+        eta = learning_schedule(epoch*m+i)
+        theta = theta - eta*gradients
+
+print("Stochastic theta:")
+print(theta)
